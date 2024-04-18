4f1b92456e881f1c4c5ed5327b1e8e5a4de155ef
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-18 21:25:39 +0800

    fixing the inference checker

--- Facial_Recog/inference_image_checker.sh
+++ Facial_Recog/inference_image_checker.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Check if the Python script is already running
-if ps aux | grep -v grep | grep "python inference_image.py --modeldir='' --imagedir='../Face_Detect/face_detected'" > /dev/null; then
-    echo "Script is already running."
-else
-    echo "Script is not running. Starting it now..."
-    python inference_image.py --modeldir='' --imagedir='../Face_Detect/face_detected' &  # Run the Python script in the background
-    echo "Script started."
-fi
+# Define the name of your Python script and its arguments
+python_script="python inference_image.py --modeldir='' --imagedir='../Face_Detect/face_detected'"
+
+# Function to check if the Python script is running
+check_script() {
+    ps aux | grep -v grep | grep "$python_script" > /dev/null
+}
+
+# Loop to continuously check and run the script if it's not already running
+while true; do
+    if check_script; then
+        echo "Script is already running."
+    else
+        echo "Script is not running. Starting it now..."
+        $python_script &  # Run the Python script in the background
+        echo "Script started."
+    fi
+    sleep 60  # Sleep for 60 seconds before checking again
+done
