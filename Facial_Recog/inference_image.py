3ebfc155161bffe4b85e9251f62d8c55155b154e
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-18 21:03:34 +0800

    checkpoint2

--- Facial_Recog/inference_image.py
+++ Facial_Recog/inference_image.py
@@ -132,57 +132,55 @@ processed_images_folder = 'processed_images'  # Folder name for processed images
 if not os.path.exists(processed_images_folder):
     os.makedirs(processed_images_folder)
 
-while True:
 # Loop over every image and perform detection
-    for image_path in images:
-        # Check if the image has already been processed
-        if image_path in processed_images:
-            continue  # Skip this image, as it has already been processed
+for image_path in images:
+    # Check if the image has already been processed
+    if image_path in processed_images:
+        continue  # Skip this image, as it has already been processed
 
-        # Load image and resize to the expected shape [1xHxWx3]
-        image = cv2.imread(image_path)
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        imH, imW, _ = image.shape
-        image_resized = cv2.resize(image_rgb, (width, height))
-        input_data = np.expand_dims(image_resized, axis=0)
+    # Load image and resize to the expected shape [1xHxWx3]
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    imH, imW, _ = image.shape
+    image_resized = cv2.resize(image_rgb, (width, height))
+    input_data = np.expand_dims(image_resized, axis=0)
 
-        if floating_model:
-            input_data = (np.float32(input_data) - input_mean) / input_std
+    if floating_model:
+        input_data = (np.float32(input_data) - input_mean) / input_std
 
-        interpreter.set_tensor(input_details[0]['index'], input_data)
-        interpreter.invoke()
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
 
-        boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]  # Bounding box coordinates of detected objects
-        classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]  # Class index of detected objects
-        scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]  # Confidence of detected objects
-        
-        for i in range(len(scores)):
-            if 0 <= int(classes[i]) < len(labels) and (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
-                object_name = labels[int(classes[i])]  # Look up object name from the "labels" array using the class index
+    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]  # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]  # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]  # Confidence of detected objects
+    
+    for i in range(len(scores)):
+        if 0 <= int(classes[i]) < len(labels) and (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
+            object_name = labels[int(classes[i])]  # Look up object name from the "labels" array using the class index
+            
+            if object_name == "Lord John Perucho":
+                now = datetime.datetime.now()
+                timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")  # YYYY-MM-DD_HH-MM-SS format
+                ymin = int(max(1, (boxes[i][0] * imH)))
+                xmin = int(max(1, (boxes[i][1] * imW)))
+                ymax = int(min(imH, (boxes[i][2] * imH)))
+                xmax = int(min(imW, (boxes[i][3] * imW)))
+                cropped_image = image[ymin:ymax, xmin:xmax]
+
+                # Resize the cropped image to the desired size (320x320)
+                cropped_image_resized = cv2.resize(cropped_image, (320, 320))
+
+                # Save the resized cropped image
+                image_name = f"{timestamp}_{object_name} ({lord_john_perucho_counter}).jpg"
+                image_path_processed = os.path.join(save_folder1, image_name)
+                cv2.imwrite(image_path_processed, cropped_image_resized)  # Capture the frame
+                lord_john_perucho_counter += 1
                 
-                if object_name == "Lord John Perucho":
-                    now = datetime.datetime.now()
-                    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")  # YYYY-MM-DD_HH-MM-SS format
-                    ymin = int(max(1, (boxes[i][0] * imH)))
-                    xmin = int(max(1, (boxes[i][1] * imW)))
-                    ymax = int(min(imH, (boxes[i][2] * imH)))
-                    xmax = int(min(imW, (boxes[i][3] * imW)))
-                    cropped_image = image[ymin:ymax, xmin:xmax]
-
-                    # Resize the cropped image to the desired size (320x320)
-                    cropped_image_resized = cv2.resize(cropped_image, (320, 320))
-
-                    # Save the resized cropped image
-                    image_name = f"{timestamp}_{object_name} ({lord_john_perucho_counter}).jpg"
-                    image_path_processed = os.path.join(save_folder1, image_name)
-                    cv2.imwrite(image_path_processed, cropped_image_resized)  # Capture the frame
-                    print("Resized and cropped image captured and saved!")
-                    lord_john_perucho_counter += 1
-                    
-                    # .Move the processed image to the processed_images folder
-                    shutil.move(image_path, os.path.join(processed_images_folder, os.path.basename(image_path)))
-                    processed_images.add(image_path)
+                # .Move the processed image to the processed_images folder
+                shutil.move(image_path, os.path.join(processed_images_folder, os.path.basename(image_path)))
+                processed_images.add(image_path)
 
 
-    # Clean up
-    cv2.destroyAllWindows()
+# Clean up
+cv2.destroyAllWindows()
