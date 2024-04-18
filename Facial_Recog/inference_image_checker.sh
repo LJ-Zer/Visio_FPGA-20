ceec816ad5e29662414cd8dd255d48553e5ded09
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-18 21:21:18 +0800

    fixing the inference checker

--- Facial_Recog/inference_image_checker.sh
+++ Facial_Recog/inference_image_checker.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Define the name of your Python script
-python_script="python"
-script_args="inference_image.py --modeldir='' --imagedir='../Face_Detect/face_detected'"
-
 # Check if the Python script is already running
-if ps aux | grep -v grep | grep "$python_script $script_args" > /dev/null; then
+if ps aux | grep -v grep | grep "python inference_image.py --modeldir='' --imagedir='../Face_Detect/face_detected'" > /dev/null; then
     echo "Script is already running."
 else
     echo "Script is not running. Starting it now..."
-    $python_script $script_args &  # Run the Python script in the background
+    python inference_image.py --modeldir='' --imagedir='../Face_Detect/face_detected' &  # Run the Python script in the background
     echo "Script started."
 fi
