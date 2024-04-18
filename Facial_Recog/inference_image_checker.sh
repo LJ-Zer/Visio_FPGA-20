3c58a75099f43daa2a071c3cf693d11f64b2f961
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-18 21:09:35 +0800

    added inference checker

---
+++ Facial_Recog/inference_image_checker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the name of your Python script
+python_script="python inference_image.py --modeldir"" --imagedir="../Face_Detect/face_detected""
+
+# Check if the Python script is already running
+if ps aux | grep -v grep | grep "$python_script" > /dev/null; then
+    echo "Script is already running."
+else
+    echo "Script is not running. Starting it now..."
+    python3 "$python_script" &  # Run the Python script in the background
+    echo "Script started."
+fi
