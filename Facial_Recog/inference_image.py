17395c3f56b465db16322d3ec07494fb6d52bcf4
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-20 20:59:25 +0800

    test for algorithm_1

--- Facial_Recog/inference_image.py
+++ Facial_Recog/inference_image.py
@@ -184,14 +184,7 @@ for image_path in images:
                     lord_john_perucho_counter += 1
                     lord_john_perucho_detected = True  # Set flag to True after first detection
                     lord_john_perucho_cooldown = time.time() + 0.9  # Block the third if statement for 60 seconds
-
-                # # .Move the processed image to the processed_images folder
-                # shutil.move(image_path, os.path.join(processed_images_folder, os.path.basename(image_path)))
-                # processed_images.add(image_path)
-            if (time.time() >= lord_john_perucho_cooldown):
-                print ("Times Up!")
-
-            if object_name == "Lord John Perucho" and lord_john_perucho_counter == 3 and (time.time() >= lord_john_perucho_cooldown): ##time.localtime().tm_hour == 17 and time.localtime().tm_min >= 12
+            elif object_name == "Lord John Perucho" and lord_john_perucho_counter == 3 and (time.time() >= lord_john_perucho_cooldown): ##time.localtime().tm_hour == 17 and time.localtime().tm_min >= 12
                     now = datetime.datetime.now()
                     timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")  # YYYY-MM-DD_HH-MM-SS format
                     ymin = int(max(1, (boxes[i][0] * imH)))
@@ -209,11 +202,6 @@ for image_path in images:
                     cv2.imwrite(image_path_processed, cropped_image_resized)  # Capture the frame
                     lord_john_perucho_detected = True 
                     lord_john_perucho_cooldown = time.time() + 1  # Block the third if statement for 60 seconds
-
-                    # # .Move the processed image to the processed_images folder
-                    # shutil.move(image_path, os.path.join(processed_images_folder, os.path.basename(image_path)))
-                    # processed_images.add(image_path)
-
             elif object_name == "Lord John Perucho":
                 now = datetime.datetime.now()
                 timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")  # YYYY-MM-DD_HH-MM-SS format
