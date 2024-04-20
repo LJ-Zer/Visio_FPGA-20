37e7e664f12c018d6cff391108bdcf8964f2e81a
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-20 21:24:35 +0800

    test for algorithm_1

--- Facial_Recog/inference_image.py
+++ Facial_Recog/inference_image.py
@@ -184,8 +184,8 @@ for image_path in images:
                     lord_john_perucho_counter += 1
                     lord_john_perucho_detected = True  # Set flag to True after first detection
                     lord_john_perucho_cooldown = time.time() + 5  # Block the third if statement for 60 seconds
-                    print (lord_john_perucho_cooldown)
-                    print (time.time() + 5)
+                    # print (lord_john_perucho_cooldown)
+                    print (time.monotonic())
             elif object_name == "Lord John Perucho" and lord_john_perucho_counter == 3 and (time.time() >= lord_john_perucho_cooldown): ##time.localtime().tm_hour == 17 and time.localtime().tm_min >= 12
                     now = datetime.datetime.now()
                     timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")  # YYYY-MM-DD_HH-MM-SS format
@@ -218,8 +218,8 @@ for image_path in images:
                 # .Move the processed image to the processed_images folder
                 shutil.move(image_path, os.path.join(processed_images_folder, os.path.basename(image_path)))
                 processed_images.add(image_path)
-                print (lord_john_perucho_cooldown)
-                print (time.time() + 5)
+                # print (lord_john_perucho_cooldown)
+                print (time.monotonic())
                 
 
 
