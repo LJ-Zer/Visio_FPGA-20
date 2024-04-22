3881edf649f6e32b8205ce3b90f691ea2cb8e0bc
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-22 15:29:22 +0800

    fix some issues for pushing images

--- Git.sh
+++ Git.sh
@@ -1,3 +1,3 @@
-export GIT_TOKEN=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
+export GIT_TOKEN=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
 cd Facial_Recog
 python Git_Upload.py
