edf53af2ada79a9c1430fee6bffaf9ea505defb0
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-22 10:35:18 +0800

    running both scripts in parallel

--- Face_Detect.sh
+++ Face_Detect.sh
@@ -1,6 +1,6 @@
 sudo xlnx-config --xmutil loadapp nlp-smartvision
 visiocutie
-source build.sh
-cd Face_Detect
-sudo ./test_video_facedetect densebox_640_360.xmodel 0
-visiocutie
+# source build.sh
+# cd Face_Detect
+# sudo ./test_video_facedetect densebox_640_360.xmodel 0
+# visiocutie
