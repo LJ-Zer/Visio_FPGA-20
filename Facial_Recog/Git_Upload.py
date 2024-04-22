ad392cc0430a6a621beb515ce032417061e19e7e
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-22 13:28:22 +0800

    .

--- Facial_Recog/Git_Upload.py
+++ Facial_Recog/Git_Upload.py
@@ -11,31 +11,19 @@ subprocess.run(['git', 'config', '--global', 'user.email', '20-06391@g.batstate-
 subprocess.run(['git', 'config', '--global', 'user.name', 'Zer-000'])
 
 try:
-    while True:
-      
-        # Change to the repository directory
-        os.chdir(repository_path)
+  git_token = os.environ.get('GIT_TOKEN')
+  if not git_token:
+    print("Error: GIT_TOKEN environment variable not set. Please set it with your PAT.")
+    sys.exit(1)
 
-        # Pull the latest changes from the remote repository
-        subprocess.run(['git', 'pull'])
+  while True:
+    # Change to the repository directory
+    os.chdir(repository_path)
 
-        # Get a list of all files in the directory
-        files = os.listdir(repository_path)
+    # Pull the latest changes from the remote repository
+    subprocess.run(['git', 'pull', 'origin', 'master'], env={'GIT_TOKEN': git_token})
 
-        # Iterate over each file and add, commit, and push
-        for file in files:
-            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
-                # Add the specific photo file to the repository's staging area
-                subprocess.run(['git', 'add', '.', file])
-
-                # Commit the changes with a commit message
-                subprocess.run(['git', 'commit', '-m', f'Upload {file}'])
-
-                # Push the changes to the remote repository on GitHub
-                subprocess.run(['git', 'push'])
-
-        # Add a delay of 5 seconds before the next iteration
-        time.sleep(5)
+    # ... (rest of your script)
 
 except KeyboardInterrupt:
-    sys.exit()
+  sys.exit()
