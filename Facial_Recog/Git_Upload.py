c12e9c21ae0d2fb6dfc1e732eda47824db450699
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-22 13:38:57 +0800

    added git_upload

--- Facial_Recog/Git_Upload.py
+++ Facial_Recog/Git_Upload.py
@@ -11,7 +11,7 @@ subprocess.run(['git', 'config', '--global', 'user.email', '20-06391@g.batstate-
 subprocess.run(['git', 'config', '--global', 'user.name', 'Zer-000'])
 
 try:
-  git_token = os.environ.get('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')
+  git_token = os.environ.get('GIT_TOKEN')
   if not git_token:
     print("Error: GIT_TOKEN environment variable not set. Please set it with your PAT.")
     sys.exit(1)
