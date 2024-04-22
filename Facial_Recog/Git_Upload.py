f8282f5d83bdba88cee47113e3fd4256e6807c81
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-22 15:22:06 +0800

    fix some issues for pushing images

--- Facial_Recog/Git_Upload.py
+++ Facial_Recog/Git_Upload.py
@@ -7,8 +7,8 @@ import time
 repository_path = "/home/ubuntu/Desktop/Visio_FPGA/Facial_Recog/Face-Detected"
 
 # Configure Git with user name and email
-# subprocess.run(['git', 'config', '--global', 'user.email', '20-06391@g.batstate-u.edu.ph'])
-# subprocess.run(['git', 'config', '--global', 'user.name', 'Zer-000'])
+subprocess.run(['git', 'config', '--global', 'user.email', '20-06391@g.batstate-u.edu.ph'])
+subprocess.run(['git', 'config', '--global', 'user.name', 'Zer-000'])
 
 try:
   git_token = os.environ.get('GIT_TOKEN')
@@ -17,6 +17,8 @@ try:
     sys.exit(1)
 
   while True:
+    subprocess.run(['git', 'config', '--global', 'user.email', '20-06391@g.batstate-u.edu.ph'])
+    subprocess.run(['git', 'config', '--global', 'user.name', 'Zer-000'])
     GIT_TOKEN = 'GIT_TOKEN'
     # Change to the repository directory
     os.chdir(repository_path)
@@ -25,14 +27,14 @@ try:
     subprocess.run(['git', 'pull', 'origin', 'master'], env={'GIT_TOKEN': git_token})
 
     # Add all tracked files (including new images)
-    # subprocess.run(['git', 'add', '.'])
+    subprocess.run(['git', 'add', '.'])
 
-    # # Commit changes with a descriptive message (consider adding timestamp)
-    # commit_message = f"Automatic Upload - {time.strftime('%Y-%m-%d %H:%M:%S')}"
-    # subprocess.run(['git', 'commit', '-m', commit_message])
+    # Commit changes with a descriptive message (consider adding timestamp)
+    commit_message = f"Automatic Upload - {time.strftime('%Y-%m-%d %H:%M:%S')}"
+    subprocess.run(['git', 'commit', '-m', commit_message])
 
-    # # Push changes to the remote repository
-    # subprocess.run(['git', 'push', 'origin', 'master'], env={'GIT_TOKEN': git_token})
+    # Push changes to the remote repository
+    subprocess.run(['git', 'push', 'origin', 'master'], env={'GIT_TOKEN': git_token})
 
     # Add a delay of 5 seconds before the next iteration
     time.sleep(5)
