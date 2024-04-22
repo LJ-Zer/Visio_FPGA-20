b2a4fc78228e6477e718b52c6c715480d8fc6398
Zer-000 <20-06391@g.batstate-u.edu.ph>
2024-04-22 15:03:30 +0800

    fix some issues for pushing images

--- Facial_Recog/Git_Upload.py
+++ Facial_Recog/Git_Upload.py
@@ -11,7 +11,7 @@ subprocess.run(['git', 'config', '--global', 'user.email', '20-06391@g.batstate-
 subprocess.run(['git', 'config', '--global', 'user.name', 'Zer-000'])
 
 try:
-  git_token = os.environ.get('GIT_TOKEN')
+  git_token = os.environ.get('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')
   if not git_token:
     print("Error: GIT_TOKEN environment variable not set. Please set it with your PAT.")
     sys.exit(1)
@@ -21,7 +21,7 @@ try:
     os.chdir(repository_path)
 
     # Pull the latest changes from the remote repository
-    subprocess.run(['git', 'pull', 'origin', 'master'], env={'GIT_TOKEN': git_token})
+    subprocess.run(['git', 'pull', 'origin', 'master'], env={'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': git_token})
 
     # Add all tracked files (including new images)
     subprocess.run(['git', 'add', '.'])
@@ -31,7 +31,7 @@ try:
     subprocess.run(['git', 'commit', '-m', commit_message])
 
     # Push changes to the remote repository
-    subprocess.run(['git', 'push', 'origin', 'master'], env={'GIT_TOKEN': git_token})
+    subprocess.run(['git', 'push', 'origin', 'master'], env={'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': git_token})
 
     # Add a delay of 5 seconds before the next iteration
     time.sleep(5)
